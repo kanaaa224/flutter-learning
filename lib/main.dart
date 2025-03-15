c52b65f5aba69d710c3df9276f8086687a0b0697
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-03-15 22:59:36 +0900

    デバッグの表示を無効化

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
             ),
             themeMode: ThemeMode.system,
             home: const MyHomePage(title: 'Flutter チュートリアル'),
+            debugShowCheckedModeBanner: false,
         );
     }
 }
